871d3b8c09b615e5576e4b17f1277b0a7a051288
Alexey Nesterov <alexey.nesterov@live.com>
2018-07-17 18:03:37 +0100

    Enable CORS

--- cakefactory/src/main/java/com/example/cakefactory/CakesController.java
+++ cakefactory/src/main/java/com/example/cakefactory/CakesController.java
@@ -2,13 +2,11 @@ package com.example.cakefactory;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/cake")
 public class CakesController {
