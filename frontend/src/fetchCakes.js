0f01e3e4d941b2c9a6186b4f712a704224fbbdf9
Alexey Nesterov <alexey.nesterov@live.com>
2018-07-17 18:03:55 +0100

    Add PCF support to the front-end app

--- frontend/src/fetchCakes.js
+++ frontend/src/fetchCakes.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.API_URL || 'http://localhost:8080';
+const baseUrl = process.env.API_URL || 'https://cakefactory.pcfbeta.io';
 
 export function fetchCakes() {
   return fetch(`${baseUrl}/cake`).then(r => r.json())
