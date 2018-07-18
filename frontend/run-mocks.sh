0ecb094a16d561cb2cf0bf8412c537586d504bc7
Alexey Nesterov <alexey.nesterov@live.com>
2018-07-18 17:05:55 +0100

    Download stub runner on first launch
    File size is too big to keep it in git.

--- frontend/run-mocks.sh
+++ frontend/run-mocks.sh
@@ -3,6 +3,12 @@
 nc -z localhost 8081
 CONNECT_TO_STUB_RUNNER=$?
 
+if [ ! -s stub-runner.jar ]
+then
+    echo "Downloading stub-runner first..."
+    wget -O stub-runner.jar 'https://search.maven.org/remote_content?g=org.springframework.cloud&a=spring-cloud-contract-stub-runner-boot&v=2.0.0.RELEASE'
+fi
+
 if [ ${CONNECT_TO_STUB_RUNNER} -eq 0 ]
 then
     echo "Stub runner is started already"
